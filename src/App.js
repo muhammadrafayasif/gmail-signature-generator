e62d45ff81f6a4eb11a6b055f4b602410d748a17
muhamamdrafayasif <muhammadrafay3561@gmail.com>
2025-08-13 14:52:39 +0300

    UI Improvements & Social Link Addition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Signature from './Signature';
 import {Form, ImageForm} from './Form';
+import './App.css';
 
 export default function App() {
   const [nameValue, setNameValue] = useState('');
@@ -9,35 +10,73 @@ export default function App() {
   const [phoneValue, setPhoneValue] = useState('');
   const [imageSrc, setImageSrc] = useState(null);
 
-  return (<>
-        <div>
-        <Signature name={nameValue} header={headerValue} email={emailValue} phone={phoneValue} image={imageSrc} />
-        </div>
-        <br />
+  const [linkedinValue, setLinkedInValue] = useState('');
+  const [githubValue, setGitHubValue] = useState('');
+  const [whatsappValue, setWhatsAppValue] = useState('');
+  const [instagramValue, setInstagramValue] = useState('');
 
-        <Form
-            value={nameValue}
-            type="name"
-            onChange={(e) => setNameValue(e.target.value)}
-        />
-        <Form
-            value={headerValue}
-            type="header"
-            onChange={(e) => setHeaderValue(e.target.value)}
-        />
-        <Form
-            value={emailValue}
-            type="email"
-            onChange={(e) => setEmailValue(e.target.value)}
-        />
-        <Form
-            value={phoneValue}
-            type="phone"
-            onChange={(e) => setPhoneValue(e.target.value)}
-        />
-        <ImageForm 
-            onImageChange={setImageSrc}
-        />
+  return (<>
+        <div className='page'>
+            <div className='gmail-signature-generator'>
+                <Signature 
+                    name={nameValue} 
+                    header={headerValue} 
+                    email={emailValue} 
+                    phone={phoneValue} 
+                    image={imageSrc}
+
+                    linkedin={linkedinValue}
+                    github={githubValue}
+                    whatsapp={whatsappValue}
+                    instagram={instagramValue}
+                />
+                <br />
+                <Form
+                    value={nameValue}
+                    type="name"
+                    onChange={(e) => setNameValue(e.target.value)}
+                />
+                <Form
+                    value={headerValue}
+                    type="header"
+                    onChange={(e) => setHeaderValue(e.target.value)}
+                />
+                <Form
+                    value={phoneValue}
+                    type="phone"
+                    onChange={(e) => setPhoneValue(e.target.value)}
+                />
+                <Form
+                    value={emailValue}
+                    type="email"
+                    onChange={(e) => setEmailValue(e.target.value)}
+                />
+                <ImageForm 
+                    onImageChange={setImageSrc}
+                />
+                <br/><br/>
+                <Form
+                    value={linkedinValue}
+                    type="LinkedIn"
+                    onChange={(e) => setLinkedInValue(e.target.value)}
+                />
+                <Form
+                    value={githubValue}
+                    type="GitHub"
+                    onChange={(e) => setGitHubValue(e.target.value)}
+                />
+                <Form
+                    value={whatsappValue}
+                    type="WhatsApp"
+                    onChange={(e) => setWhatsAppValue(e.target.value)}
+                />
+                <Form
+                    value={instagramValue}
+                    type="Instagram"
+                    onChange={(e) => setInstagramValue(e.target.value)}
+                />
+            </div>
+        </div>
     </>
   );
 }
--- src/Signature.js
+++ src/Signature.js
@@ -1,36 +1,81 @@
-function Signature({ name, header, phone, email, image }) {
+function Signature({ 
+  name, 
+  header, 
+  phone, 
+  email, 
+  image,
+
+  linkedin,
+  github,
+  whatsapp,
+  instagram
+}) {
   const cellStyle = {
     border: "none",
     padding: '3px 10px',
     whiteSpace: 'nowrap',
-    // verticalAlign: 'middle',
   };
+  const linkStyle = {
+    marginRight: "5px"
+  }
   return (
       <>
         <p>Best Regards,</p>
         <table border="1" style={{ borderCollapse: "collapse", border: "none", backgroundColor: "#F5F5F5", borderRadius: "14px" }}>
           <tr style={{border: "none"}}>
-            {image ? (<td style={{...cellStyle, padding: "5px 10px"}} rowspan="5">
-              <img style={{ borderRadius: "10px" }} alt='profile_picture' width='80' src={image} />
-            </td>) : (<td style={{...cellStyle, padding: "5px 10px"}} rowspan="5">
-              <img style={{ borderRadius: "10px" }} alt='profile_picture' width='80' src="https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg" />
-            </td>)}
-            <td style={{...cellStyle, fontWeight: "bold" }} colspan="6">{name}</td>
+            {
+              image ? 
+                (<td style={{...cellStyle, padding: "5px 10px"}} rowspan="5">
+                  <img style={{ borderRadius: "10px" }} alt='profile_picture' width='80' src={image} />
+                </td>) 
+                : 
+                (<td style={{...cellStyle, padding: "5px 10px"}} rowspan="5">
+                  <img style={{ borderRadius: "10px" }} alt='profile_picture' width='80' src="https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg" />
+                </td>)
+              }
+            <td style={{...cellStyle, fontWeight: "bold" }} colspan="6">{name ? name : 'Muhammad Falan'}</td>
           </tr>
-          {header && (<tr style={{border: "none"}}>
-            <td style={cellStyle} colspan="6"><i>{header}</i></td>
-          </tr>)}
+          {
+            header && 
+            (<tr style={{border: "none"}}>
+              <td style={cellStyle} colspan="6"><i>{header}</i></td>
+            </tr>)
+          }
+          {
+            phone && 
+            (<tr style={{border: "none"}}>
+              <td style={cellStyle} colspan="6">📞<i> <a href={`tel:${phone.replace(/\s+/g, '')}`} style={{textDecoration: "none", color: "black"}}>{phone}</a></i></td>
+            </tr>)
+          }
+          {
+            email && 
+            (<tr style={{border: "none"}}>
+              <td style={cellStyle} colspan="6">📧<i> <a href={`mailto:${email}`} style={{color: "black"}}>{email}</a></i></td>
+            </tr>)
+          }
           <tr style={{border: "none"}}>
-            <td style={cellStyle} colspan="6">📞<i> {phone}</i></td>
-          </tr>
-          <tr style={{border: "none"}}>
-            <td style={cellStyle} colspan="6">📧<i> {email}</i></td>
-          </tr>
-          <tr style={{border: "none"}}>
-            <td style={cellStyle}><a href="https://www.linkedin.com"><img width="30" alt="LinkedIn" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" /></a></td>
-            <td style={cellStyle}><a href="https://www.github.com"><img width="26" alt="GitHub" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Font_Awesome_5_brands_github-square.svg/512px-Font_Awesome_5_brands_github-square.svg.png?20181017221158" /></a></td>
-            <td style={cellStyle}><a href="https://www.whatsapp.com"><img width="30" alt="WhatsApp" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1022px-WhatsApp.svg.png" /></a></td>
-            <td style={cellStyle}><a href="https://www.instagram.com"><img width="30" alt="Instagram" src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" /></a></td>
+            <td style={{...cellStyle, margin: '10px'}}>
+            
+            {
+              linkedin && 
+              (<a style={linkStyle} href={linkedin}><img width="30" alt="LinkedIn" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" /></a>)
+            }
+
+            {
+              github && 
+              (<a style={linkStyle} href={github}><img width="26" alt="GitHub" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Font_Awesome_5_brands_github-square.svg/512px-Font_Awesome_5_brands_github-square.svg.png?20181017221158" /></a>)
+            }
+
+            {
+              whatsapp && 
+              (<a style={linkStyle} href={whatsapp}><img width="30" alt="WhatsApp" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1022px-WhatsApp.svg.png" /></a>)
+            }
+
+            {
+              instagram && 
+              (<a style={linkStyle} href={instagram}><img width="30" alt="Instagram" src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" /></a>)
+            }
+            </td>
           </tr>
         </table>
       </>
